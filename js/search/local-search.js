7ab7b1c0917b4805198d3d864dfb5c36020ffca8
WangQianyu6 <qianyu.wang@emqx.io>
2023-07-14 19:29:16 +0800

    Site updated: 2023-07-14 19:29:16

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -330,7 +330,7 @@ window.addEventListener('load', () => {
   }
 
   const searchClickFn = () => {
-    document.querySelector('#search-button > .search').addEventListener('click', openSearch)
+    document.querySelector('#menu-search').addEventListener('click', openSearch)
   }
 
   const searchFnOnce = () => {
